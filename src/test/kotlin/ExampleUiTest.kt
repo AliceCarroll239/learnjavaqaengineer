a53d027da86be465a605a4cfe4f3d24e3433441d
alexeevatatyana <tatyana@disrapp.com>
2019-06-21 13:25:13 +0300

    Fill in forms for trianle QA test

--- src/test/kotlin/ExampleUiTest.kt
+++ src/test/kotlin/ExampleUiTest.kt
@@ -4,19 +4,39 @@ import org.junit.jupiter.api.*
 import org.openqa.selenium.WebDriver
 import utils.TestUtils
 import org.junit.jupiter.api.TestInstance.Lifecycle
+import org.openqa.selenium.By
+import utils.Numbers
 
 @TestInstance(Lifecycle.PER_CLASS)
 class ExampleUiTest {
+
     private val driver: WebDriver = TestUtils().configureFirefoxWebDriver(TestUtils().getCurrentWorkingDirectory())
 
     @Test
     fun checkDriver() {
-        driver.get("https://www.google.com/")
-        assertThat(driver.currentUrl, `is`("https://www.google.com/"))
+        driver.get("https://playground.learnqa.ru/puzzle/triangle/")
+        val minimizeButton = driver.findElement(By.cssSelector("[onclick]"))
+        minimizeButton.click()
+
+        val inputA = driver.findElement(By.cssSelector(".js_a"))
+        val inputB = driver.findElement(By.cssSelector(".js_b"))
+        val inputC = driver.findElement(By.cssSelector(".js_c"))
+        val submit = driver.findElement(By.cssSelector(".btn-submit"))
+        var result = ""
+
+        Numbers().fillForm(inputA, "100", inputB, "100", inputC, "100", submit)
+        result = driver.findElement(By.cssSelector(".info")).text
+        assertThat(result, `is`("Это равносторонний треугольник.\nВы ввели:\nA: 100; B: 100; C: 100"))
+
+        Numbers().fillForm(inputA, "10", inputB, "10", inputC, "5", submit)
+        result = driver.findElement(By.cssSelector(".info")).text
+        assertThat(result, `is`("Это равнобедренный треугольник.\nВы ввели:\nA: 10; B: 10; C: 5"))
+
+
     }
 
-    @AfterAll
-    fun driverClose() {
-        driver.close()
-    }
+//    @AfterAll
+//    fun driverClose() {
+//        driver.close()
+//    }
 }
---
+++ src/test/kotlin/utils/Numbers.kt
@@ -0,0 +1,25 @@
+package utils
+
+import org.openqa.selenium.WebElement
+
+class Numbers {
+    fun fillForm(
+        inputA: WebElement, sendA: String,
+        inputB: WebElement, sendB: String,
+        inputC: WebElement, sendC: String,
+        submit: WebElement
+    ) {
+        inputA.clear()
+        inputB.clear()
+        inputC.clear()
+        inputA.sendKeys(sendA)
+        inputB.sendKeys(sendB)
+        inputC.sendKeys(sendC)
+        Thread.sleep(2000)
+        submit.click()
+    }
+
+    fun compare(){
+
+    }
+}
