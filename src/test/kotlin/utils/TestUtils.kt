4ca1f627e16b4d494365d5a6386b4901184c2357
DmitryNoraev <dmitrynoraev@gmail.com>
2019-06-24 14:41:51 +0300

    OS

--- src/test/kotlin/utils/TestUtils.kt
+++ src/test/kotlin/utils/TestUtils.kt
@@ -19,23 +19,28 @@ class TestUtils {
         val firefoxOptions = FirefoxOptions()
         val profile = FirefoxProfile()
 
-        //TODO Определение ОС
-        driverPath = driverPath.resolve("src/test/resources/libs/geckodriver.exe")
+        val OS = System.getProperty("os.name").toLowerCase()
+        if (OS.indexOf("win") >= 0) {
+            driverPath = driverPath.resolve("src/test/resources/libs/geckodriver.exe")
+        } else driverPath = driverPath.resolve("src/test/resources/libs/geckodriver")
+
         System.setProperty("webdriver.gecko.driver", driverPath.toAbsolutePath().toString())
         firefoxOptions.binary = firefoxBinary
-        profile.setPreference("browser.download.folderList", 2)
-        profile.setPreference("browser.download.dir", "C:\\Test\\")
-        profile.setPreference("browser.download.manager.alertOnEXEOpen", false)
-        profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "image/x-icon")
-        profile.setPreference("browser.download.manager.showWhenStarting", false)
-        profile.setPreference("browser.download.manager.focusWhenStarting", false)
-        profile.setPreference("browser.download.useDownloadDir", true)
-        profile.setPreference("browser.helperApps.alwaysAsk.force", false)
-        profile.setPreference("browser.download.manager.alertOnEXEOpen", false)
-        profile.setPreference("browser.download.manager.closeWhenDone", true)
-        profile.setPreference("browser.download.manager.showAlertOnComplete", false)
-        profile.setPreference("browser.download.manager.useWindow", false)
-        profile.setPreference("services.sync.prefs.sync.browser.download.manager.showWhenStarting", false)
+
+        //------- Пока не надо ---------//
+//        profile.setPreference("browser.download.folderList", 2)
+//        profile.setPreference("browser.download.dir", "C:\\Test\\")
+//        profile.setPreference("browser.download.manager.alertOnEXEOpen", false)
+//        profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "image/x-icon")
+//        profile.setPreference("browser.download.manager.showWhenStarting", false)
+//        profile.setPreference("browser.download.manager.focusWhenStarting", false)
+//        profile.setPreference("browser.download.useDownloadDir", true)
+//        profile.setPreference("browser.helperApps.alwaysAsk.force", false)
+//        profile.setPreference("browser.download.manager.alertOnEXEOpen", false)
+//        profile.setPreference("browser.download.manager.closeWhenDone", true)
+//        profile.setPreference("browser.download.manager.showAlertOnComplete", false)
+//        profile.setPreference("browser.download.manager.useWindow", false)
+//        profile.setPreference("services.sync.prefs.sync.browser.download.manager.showWhenStarting", false)
         firefoxOptions.profile = profile
 
         return FirefoxDriver(firefoxOptions)
