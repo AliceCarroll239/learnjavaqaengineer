ad11856cb32fc90584e9bc127742d46066b6d038
DmitryNoraev <dmitrynoraev@gmail.com>
2019-06-26 14:17:28 +0300

    Пареметризация в тестах

--- src/test/kotlin/ExampleUiTest.kt
+++ src/test/kotlin/ExampleUiTest.kt
@@ -1,14 +1,22 @@
 import com.google.gson.Gson
+import data.Bug
+import data.Case
 import data.TriangleDataClass
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
 import org.junit.jupiter.api.*
 import org.openqa.selenium.WebDriver
 import utils.TestUtils
 import org.junit.jupiter.api.TestInstance.Lifecycle
+import org.junit.jupiter.api.parallel.Execution
+import org.junit.jupiter.api.parallel.ExecutionMode
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import org.openqa.selenium.By
 import utils.DataProvider
 import utils.Numbers
+import java.util.concurrent.TimeUnit
 
 @TestInstance(Lifecycle.PER_CLASS)
 class ExampleUiTest {
@@ -20,37 +28,44 @@ class ExampleUiTest {
             .resolve("src/test/resources/params.json"))
     private val testSettings = Gson().fromJson(paramsFile, TriangleDataClass::class.java)
 
-    @Test
-    fun checkDriver() {
-        //Костыль
-        //JavaScript error: https://playground.learnqa.ru/js/puzzle/jquery-3.2.1.min.js,
-        // line 5: TypeError: document.querySelector(...) is null
+    @BeforeAll
+    fun openPage() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
         driver.manage().window().maximize()
         for (i in 1..2) {
             driver.get("https://playground.learnqa.ru/puzzle/triangle/")
             Thread.sleep(2000)
         }
-
         val minimizeButton = driver.findElement(By.cssSelector("[onclick]"))
         minimizeButton.click()
+    }
 
-        val inputA = driver.findElement(By.cssSelector(".js_a"))
-        val inputB = driver.findElement(By.cssSelector(".js_b"))
-        val inputC = driver.findElement(By.cssSelector(".js_c"))
-        val submit = driver.findElement(By.cssSelector(".btn-submit"))
+    @ParameterizedTest
+    @MethodSource("utils.Provider#getCases")
+    fun checkCases(testCase: Case) {
+        val inputA = driver.findElement(By.cssSelector(".js_a"))!!
+        val inputB = driver.findElement(By.cssSelector(".js_b"))!!
+        val inputC = driver.findElement(By.cssSelector(".js_c"))!!
+        val submit = driver.findElement(By.cssSelector(".btn-submit"))!!
         var result = ""
 
-        testSettings.data.cases.forEach {
-            Numbers().fillForm(inputA, it.a, inputB, it.b, inputC, it.c, submit)
-            result = driver.findElement(By.cssSelector(".info")).text
-            assertThat(result, `is`(it.expectedResult))
-        }
+        Numbers().fillForm(inputA, testCase.a, inputB, testCase.b, inputC, testCase.c, submit)
+        result = driver.findElement(By.cssSelector(".info")).text
+        assertThat(result, `is`(testCase.expectedResult))
+    }
 
-        testSettings.data.bugs.forEach {
-            Numbers().fillForm(inputA, it.a, inputB, it.b, inputC, it.c, submit)
-            result = driver.findElement(By.cssSelector(".info")).text
-            assertThat(result, `is`(it.expectedResult))
-        }
+    @ParameterizedTest
+    @MethodSource("utils.Provider#getBugs")
+    fun checkBugs(testCase: Bug) {
+        val inputA = driver.findElement(By.cssSelector(".js_a"))!!
+        val inputB = driver.findElement(By.cssSelector(".js_b"))!!
+        val inputC = driver.findElement(By.cssSelector(".js_c"))!!
+        val submit = driver.findElement(By.cssSelector(".btn-submit"))!!
+        var result = ""
+
+        Numbers().fillForm(inputA, testCase.a, inputB, testCase.b, inputC, testCase.c, submit)
+        result = driver.findElement(By.cssSelector(".info")).text
+        assertThat(result, `is`(testCase.expectedResult))
     }
 
     @AfterAll
